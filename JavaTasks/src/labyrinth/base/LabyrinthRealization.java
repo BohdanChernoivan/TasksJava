1dbedd6c72b66124b2a112fbc790e0c3e7e663a6
BohdanChernoivan <scoliztur@gmail.com>
2019-07-12 22:06:45 +0300

    HW

---
+++ JavaTasks/src/labyrinth/base/LabyrinthRealization.java
@@ -0,0 +1,10 @@
+package labyrinth.base;
+
+public abstract class LabyrinthRealization {
+
+    protected abstract void createLabyrinth();
+
+    protected abstract void showLabyrinth();
+
+    protected abstract boolean passingTheLabyrinth();
+}
---
+++ JavaTasks/src/labyrinth/details/LabyrinthPoint.java
@@ -0,0 +1,22 @@
+package labyrinth.details;
+
+public enum LabyrinthPoint {
+    FREE_WAY(" "),
+//    PASSAGE("[*]"),
+    WALL("*"),
+    ENTRY("\u2386"),
+    EXIT("\uD83D\uDEAA"),
+    THESEUS("\uD83D\uDEB6"),
+    MINOTAUR("\uD800\uDDEB")
+    ;
+
+    private String item;
+
+    LabyrinthPoint(String item) {
+        this.item = item;
+    }
+
+    public String getItem() {
+        return item;
+    }
+}
